d37edb29f3c4182f0fcc6367a7cc7f48227a010d
Chloe <44501120+chloezxyy@users.noreply.github.com>
2024-09-30 20:48:24 +0800

    fix(ui-ux): gap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <WalletProvider>
       <div className="p-5">
-        <main className="md:max-w-4xl mx-auto grid gap-y-5">
+        <main className="md:max-w-4xl mx-auto grid gap-y-6">
           <WalletDetails />
           <header className="flex flex-row w-full items-center justify-between">
             <h1 className="text-base md:text-lg font-bold">Scroll Web dApp</h1>
